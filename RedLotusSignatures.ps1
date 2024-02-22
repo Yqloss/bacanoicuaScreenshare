4722f08694e52d6fef9957b2b87e8acd6ef15a0b
bacanoicua <72052090+bacanoicua@users.noreply.github.com>
2024-02-22 17:06:58 -0300

    Update RedLotusSignatures.ps1
    Barra de carga

--- RedLotusSignatures.ps1
+++ RedLotusSignatures.ps1
@@ -6,18 +6,10 @@ Write-Host @"
   ██████╔╝█████╗  ██║  ██║    ██║     ██║   ██║   ██║   ██║   ██║███████╗         
   ██╔══██╗██╔══╝  ██║  ██║    ██║     ██║   ██║   ██║   ██║   ██║╚════██║         
   ██║  ██║███████╗██████╔╝    ███████╗╚██████╔╝   ██║   ╚██████╔╝███████║         
-  ╚═╝  ╚═╝╚══════╝╚═════╝     ╚══════╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝         
-                                                                                  
-  ███████╗██╗ ██████╗ ███╗   ██╗ █████╗ ████████╗██╗   ██╗██████╗ ███████╗███████╗
-  ██╔════╝██║██╔════╝ ████╗  ██║██╔══██╗╚══██╔══╝██║   ██║██╔══██╗██╔════╝██╔════╝
-  ███████╗██║██║  ███╗██╔██╗ ██║███████║   ██║   ██║   ██║██████╔╝█████╗  ███████╗
-  ╚════██║██║██║   ██║██║╚██╗██║██╔══██║   ██║   ██║   ██║██╔══██╗██╔══╝  ╚════██║
-  ███████║██║╚██████╔╝██║ ╚████║██║  ██║   ██║   ╚██████╔╝██║  ██║███████╗███████║
-  ╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝
-                                                                                  
+  ╚═╝  ╚═╝╚══════╝╚═════╝     ╚══════╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝                                                                                   
 "@ -ForegroundColor Red
 Write-Host ""
-Write-Host "  Made by Bacanoicua kjj - " -ForegroundColor Blue -NoNewLine
+Write-Host "  Made by Bacanoicua kjj - " -ForegroundColor Blue -NoNewline
 Write-Host -ForegroundColor Red "discord.gg/redlotus"
 
 Write-Host ""
@@ -38,29 +30,37 @@ if(-Not(Test-Path -Path $pathsFilePath)){
 }
 
 $paths = Get-Content "paths.txt"
-
 $stopwatch = [Diagnostics.Stopwatch]::StartNew()
 
 $results = @()
+$count = 0
+$totalCount = $paths.Count
+$progressID = 1
 
 foreach ($path in $paths) {
+    $progress = [int]($count / $totalCount * 100)
+    Write-Progress -Activity "Scanning paths..." -Status "$progress% Complete:" -PercentComplete $progress -Id $progressID
+    $count++
 
-    $fileName = Split-Path $path -Leaf
-    Write-Host $path -ForegroundColor Blue
-    $signatureStatus = (Get-AuthenticodeSignature $path 2>$null).Status
-    
-    $fileDetails = New-Object PSObject
-    $fileDetails | Add-Member Noteproperty Name $fileName
-    $fileDetails | Add-Member Noteproperty Path $path
-    $fileDetails | Add-Member Noteproperty SignatureStatus $signatureStatus
+    Try {
+        $fileName = Split-Path $path -Leaf
+        $signatureStatus = (Get-AuthenticodeSignature $path 2>$null).Status
 
-    $results += $fileDetails
+        $fileDetails = New-Object PSObject
+        $fileDetails | Add-Member Noteproperty Name $fileName
+        $fileDetails | Add-Member Noteproperty Path $path
+        $fileDetails | Add-Member Noteproperty SignatureStatus $signatureStatus
+
+        $results += $fileDetails
+    } Catch {
+    }
 }
 
-$results | Out-GridView -PassThru -Title 'Signatures Results'
 $stopwatch.Stop()
 
 $time = $stopwatch.Elapsed.Hours.ToString("00") + ":" + $stopwatch.Elapsed.Minutes.ToString("00") + ":" + $stopwatch.Elapsed.Seconds.ToString("00") + "." + $stopwatch.Elapsed.Milliseconds.ToString("000")
 
 Write-Host ""
 Write-Host "The scan took $time to run." -ForegroundColor Yellow
+
+$results | Out-GridView -PassThru -Title 'Signatures Results'
